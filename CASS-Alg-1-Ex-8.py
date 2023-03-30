da0f1078d1679fc178532bf8c8aa754fe3c6b2ef
Cecília <103593379+silvaceciliaa@users.noreply.github.com>
2023-03-30 15:23:50 -0300

    Add files via upload

---
+++ CASS-Alg-1-Ex-8.py
@@ -0,0 +1,9 @@
+bugigangas = int(input('Quantas bugigangas tem no seu pedido? '))
+quinquilharias = int(input('Quantas quinquilharias tem no seu pedido? '))
+
+pesoBugigangas = bugigangas * 75
+pesoQuinquilharias = quinquilharias * 112
+
+pesoTotal = pesoBugigangas + pesoQuinquilharias
+
+print('O peso total é de', pesoTotal, 'gramas')
