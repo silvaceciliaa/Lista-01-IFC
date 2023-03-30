5ea0ff99aa729ff793a67a90e9e323ef4faff382
Cecília <103593379+silvaceciliaa@users.noreply.github.com>
2023-03-30 16:06:02 -0300

    Add files via upload

---
+++ CASS-Alg-1-Ex-9.py
@@ -0,0 +1,9 @@
+valorInicial = float(input('Digite o valor a ser depositado: '))
+
+valorAno1 = valorInicial * 1.2
+valorAno2 = valorAno1 * 1.2
+valorAno3 = valorAno2 * 1.2
+
+print(f'O valor no primeiro ano é de: R${valorAno1:.2f}')
+print(f'O valor no segundo ano é de: R${valorAno2:.2f}')
+print(f'O valor no terceiro ano é de: R${valorAno3:.2f}')
