c49b7707b13a4b72f40a20c934be2cf5b96f2abf
Cecília <103593379+silvaceciliaa@users.noreply.github.com>
2023-04-03 15:39:47 -0300

    Add files via upload

---
+++ CASS-Alg-01-Ex-12.py
@@ -0,0 +1,11 @@
+import math
+
+raior = float(input('Digite o valor do raio: '))
+
+area = math.pi * raior ** 2
+
+print('Área:', area)
+
+volume = 4/3 * math.pi * raior ** 3
+
+print('Volume:', volume)
